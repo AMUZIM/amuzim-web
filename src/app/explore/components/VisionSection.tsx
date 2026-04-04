81c2d5943803fef97330039c8849c54c36171280
Farhad S <amuzimteam@gmail.com>
2026-04-05 00:56:54 +0200

    Create VisionSection.tsx

---
+++ src/app/explore/components/VisionSection.tsx
@@ -0,0 +1,11 @@
+export default function VisionSection() {
+  return (
+    <section>
+      <h2>Find Your Place in Music</h2>
+      <p>
+        Whether you love music, create, collaborate or work in the industry,
+        AMUZIM helps you discover, connect and grow.
+      </p>
+    </section>
+  );
+}
