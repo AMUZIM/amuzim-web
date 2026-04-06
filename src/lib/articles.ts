de460b6036b1232487756b1d9e1ba81014e84afb
Farhad S <amuzimteam@gmail.com>
2026-04-06 08:24:48 +0200

    Update articles.ts

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -1,1 +1,28 @@
+import fs from "fs"
+import path from "path"
+import matter from "gray-matter"
 
+const articlesDirectory = path.join(process.cwd(), "content/articles")
+
+export function getAllArticles() {
+  const fileNames = fs.readdirSync(articlesDirectory)
+
+  const articles = fileNames.map((fileName) => {
+    const slug = fileName.replace(".mdx", "")
+
+    const fullPath = path.join(articlesDirectory, fileName)
+    const fileContents = fs.readFileSync(fullPath, "utf8")
+
+    const { data } = matter(fileContents)
+
+    return {
+      slug,
+      ...data,
+    }
+  })
+
+  return articles.sort((a, b) =>
+    new Date(b.publishedAt).getTime() -
+    new Date(a.publishedAt).getTime()
+  )
+}
