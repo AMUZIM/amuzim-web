37c6afe416891f3e95e50e7db6465fe41707cf19
Farhad S <amuzimteam@gmail.com>
2026-04-06 08:33:42 +0200

    Update ArticlesList.tsx

--- src/components/editorial/ArticlesList.tsx
+++ src/components/editorial/ArticlesList.tsx
@@ -1,1 +1,12 @@
 
+import ArticleCard from "./ArticleCard"
+
+export default function ArticlesList({ articles }) {
+  return (
+    <section>
+      {articles.map((article) => (
+        <ArticleCard key={article.slug} article={article} />
+      ))}
+    </section>
+  )
+}
