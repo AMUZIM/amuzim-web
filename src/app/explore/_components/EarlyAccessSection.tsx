63ddcf793857f91ee1df54952ecb304234ab52d7
Farhad S <amuzimteam@gmail.com>
2026-04-08 09:09:34 +0200

    Delete src/app/explore/_components directory

--- src/app/explore/_components/EarlyAccessSection.tsx
+++
@@ -1,8 +0,0 @@
-export default function EarlyAccessSection() {
-  return (
-    <section>
-      <h2>Be Early. Shape the Ecosystem.</h2>
-      <p>Join as Artist, Professional or Music Lover</p>
-    </section>
-  );
-}
