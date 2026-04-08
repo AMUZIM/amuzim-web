135e4570c38df9075906fedebd1debe770c51912
Farhad S <amuzimteam@gmail.com>
2026-04-08 10:45:44 +0200

    Update FeaturedSection.tsx

--- src/app/explore/components/FeaturedSection.tsx
+++ src/app/explore/components/FeaturedSection.tsx
@@ -1,16 +1,8 @@
-import FeaturedArtists from "./featured/FeaturedArtists";
-import FeaturedMusic from "./featured/FeaturedMusic";
-import FeaturedOpportunities from "./featured/FeaturedOpportunities";
-
 export default function FeaturedSection() {
   return (
     <section>
       <h2>Featured</h2>
-      <p>Curated artists, music and opportunities</p>
-
-      <FeaturedArtists />
-      <FeaturedMusic />
-      <FeaturedOpportunities />
+      <p>Featured Artists, Music & Opportunities</p>
     </section>
   );
 }
