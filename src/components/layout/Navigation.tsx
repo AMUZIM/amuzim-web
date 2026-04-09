3ad5854d8f87ed05e52e25267375ca1afb6475f8
Farhad S <amuzimteam@gmail.com>
2026-04-09 07:52:11 +0200

    Update Navigation.tsx

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 export default function Navigation() {
   return (
-    <nav className="flex gap-8 text-sm">
+    <nav className="flex items-center gap-8 text-sm font-medium">
       <Link href="/explore">Explore</Link>
       <Link href="/editorial">Editorial</Link>
       <Link href="/future">Future</Link>
