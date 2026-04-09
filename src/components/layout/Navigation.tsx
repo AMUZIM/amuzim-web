6869311246a80d98c24e5a81091a960550bb2097
Farhad S <amuzimteam@gmail.com>
2026-04-09 07:44:24 +0200

    Update Navigation.tsx

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -2,15 +2,11 @@ import Link from "next/link"
 
 export default function Navigation() {
   return (
-    <nav>
-      <div>
-        <ul style={{ display: "flex", gap: "30px" }}>
-          <li><Link href="/explore">Explore</Link></li>
-          <li><Link href="/editorial">Editorial</Link></li>
-          <li><Link href="/future">Future</Link></li>
-          <li><Link href="/network">Network</Link></li>
-        </ul>
-      </div>
+    <nav className="flex gap-8 text-sm">
+      <Link href="/explore">Explore</Link>
+      <Link href="/editorial">Editorial</Link>
+      <Link href="/future">Future</Link>
+      <Link href="/network">Network</Link>
     </nav>
   )
 }
