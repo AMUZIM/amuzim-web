4aae213cbc89e51269f5125a765a07f3a3ef0fa7
Farhad S <amuzimteam@gmail.com>
2026-04-09 03:32:14 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,8 @@
-import Link from "next/link"
-
 export default function Home() {
   return (
     <main>
       <h1>AMUZIM</h1>
       <p>Music Intelligence Platform</p>
-
-      <ul>
-        <li><Link href="/explore">Explore</Link></li>
-        <li><Link href="/editorial">Editorial</Link></li>
-        <li><Link href="/future">Future</Link></li>
-        <li><Link href="/network">Network</Link></li>
-      </ul>
     </main>
   )
 }
