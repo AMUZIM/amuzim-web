b3c3be5e49dc0ecbae585caa93d481df3434cdd9
Farhad S <amuzimteam@gmail.com>
2026-04-09 03:51:00 +0200

    Update Navigation.tsx

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -3,12 +3,14 @@ import Link from "next/link"
 export default function Navigation() {
   return (
     <nav>
-      <ul>
-        <li><Link href="/explore">Explore</Link></li>
-        <li><Link href="/editorial">Editorial</Link></li>
-        <li><Link href="/future">Future</Link></li>
-        <li><Link href="/network">Network</Link></li>
-      </ul>
+      <div>
+        <ul>
+          <li><Link href="/explore">Explore</Link></li>
+          <li><Link href="/editorial">Editorial</Link></li>
+          <li><Link href="/future">Future</Link></li>
+          <li><Link href="/network">Network</Link></li>
+        </ul>
+      </div>
     </nav>
   )
 }
