677f9be0f3968f8ae864f6b7a0684547d6fb8779
Farhad S <amuzimteam@gmail.com>
2026-04-09 04:12:48 +0200

    Update Navigation.tsx

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -4,7 +4,7 @@ export default function Navigation() {
   return (
     <nav>
       <div>
-        <ul>
+        <ul style={{ display: "flex", gap: "30px" }}>
           <li><Link href="/explore">Explore</Link></li>
           <li><Link href="/editorial">Editorial</Link></li>
           <li><Link href="/future">Future</Link></li>
