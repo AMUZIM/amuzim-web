fb4e6f3297e65e690fa9901cf4b042c83dc49863
Farhad S <amuzimteam@gmail.com>
2026-04-09 10:52:54 +0200

    Update Header.tsx

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,14 @@
 import Navigation from "./Navigation"
+import Link from "next/link"
 
 export default function Header() {
   return (
-    <header className="border-b border-neutral-200">
+    <header className="border-b border-neutral-200 sticky top-0 bg-white z-50">
       <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
         
-        <div className="text-lg font-semibold">
+        <Link href="/" className="text-lg font-semibold tracking-tight">
           AMUZIM
-        </div>
+        </Link>
 
         <Navigation />
 
