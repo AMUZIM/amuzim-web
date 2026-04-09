1dda3ae7f2a81c114dfda932b48b6294b9543cd3
Farhad S <amuzimteam@gmail.com>
2026-04-09 11:58:47 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
