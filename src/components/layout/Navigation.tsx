b9c8620a7abdf9fab622daf6e5f9195542be978a
Farhad S <amuzimteam@gmail.com>
2026-04-09 02:44:55 +0200

    Update Navigation.tsx

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,1 +1,13 @@
 
+export default function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>Explore</li>
+        <li>Editorial</li>
+        <li>Future</li>
+        <li>Network</li>
+      </ul>
+    </nav>
+  )
+}
