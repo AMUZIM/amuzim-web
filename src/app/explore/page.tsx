e9b405ed4a196c03bfbb6c5d99277017a2738485
Farhad S <amuzimteam@gmail.com>
2026-04-11 01:22:39 +0200

    Update page.tsx

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,7 +1,38 @@
+import HeroExplore from "@/components/explore/HeroExplore"
+import CuratedMusic from "@/components/explore/CuratedMusic"
+import MusicLovers from "@/components/explore/MusicLovers"
+import CreatorInfrastructure from "@/components/explore/CreatorInfrastructure"
+import Learning from "@/components/explore/Learning"
+import IndustryAccess from "@/components/explore/IndustryAccess"
+import ResponsibleAI from "@/components/explore/ResponsibleAI"
+import LegalRights from "@/components/explore/LegalRights"
+import FutureInfrastructure from "@/components/explore/FutureInfrastructure"
+import ExploreCTA from "@/components/explore/ExploreCTA"
+
 export default function Explore() {
   return (
     <main>
-      <h1>Explore</h1>
+
+      <HeroExplore />
+
+      <CuratedMusic />
+
+      <MusicLovers />
+
+      <CreatorInfrastructure />
+
+      <Learning />
+
+      <IndustryAccess />
+
+      <ResponsibleAI />
+
+      <LegalRights />
+
+      <FutureInfrastructure />
+
+      <ExploreCTA />
+
     </main>
   )
 }
