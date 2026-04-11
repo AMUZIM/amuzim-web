ab9708cb70681ae1cddf006162282c61395fba50
Farhad S <amuzimteam@gmail.com>
2026-04-11 03:16:32 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,8 @@
-const config = {
+import type { Config } from "tailwindcss"
+
+const config: Config = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
