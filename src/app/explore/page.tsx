9fc333fc6f472ae754385ee509b4e9d783fa1839
Farhad S <amuzimteam@gmail.com>
2026-04-13 07:33:45 +0200

    Update page.tsx

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,12 +1,7 @@
 import HeroExplore from "../../components/HeroExplore"
 import CuratedMusic from "../../components/CuratedMusic"
 import MusicLovers from "../../components/MusicLovers"
-import CreatorInfrastructure from "../../components/CreatorInfrastructure"
 import Learning from "../../components/Learning"
-import IndustryAccess from "../../components/IndustryAccess"
-import ResponsibleAI from "../../components/ResponsibleAI"
-import LegalRights from "../../components/LegalRights"
-import FutureInfrastructure from "../../components/FutureInfrastructure"
 import ExploreCTA from "../../components/ExploreCTA"
 
 export default function Explore() {
@@ -18,12 +13,7 @@ export default function Explore() {
       <div className="space-y-32">
         <CuratedMusic />
         <MusicLovers />
-        <CreatorInfrastructure />
         <Learning />
-        <IndustryAccess />
-        <ResponsibleAI />
-        <LegalRights />
-        <FutureInfrastructure />
       </div>
 
       <ExploreCTA />
