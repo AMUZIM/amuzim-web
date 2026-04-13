e44e15f5bfe26098bf7820d75e699c37c8a63fa6
Farhad S <amuzimteam@gmail.com>
2026-04-13 16:40:16 +0200

    Update NetworkFeed.tsx

--- src/components/network/NetworkFeed.tsx
+++ src/components/network/NetworkFeed.tsx
@@ -1,1 +1,12 @@
 
+import NetworkPost from "./NetworkPost";
+
+export default function NetworkFeed() {
+  return (
+    <div className="space-y-6">
+      <NetworkPost />
+      <NetworkPost />
+      <NetworkPost />
+    </div>
+  );
+}
