0299dbe0d04af9f65ddae37cc53983e102c41646
Farhad S <amuzimteam@gmail.com>
2026-04-13 16:49:03 +0200

    Update post.ts

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,1 +1,30 @@
 
+import { UserRole } from "./user"
+
+export type PostType =
+  | "discovery"
+  | "creator"
+  | "industry"
+  | "discussion"
+  | "announcement"
+
+
+export interface Post {
+  id: string
+
+  authorId: string
+  authorName: string
+  authorRole: UserRole
+  authorAvatar?: string
+
+  content: string
+
+  type: PostType
+
+  tags?: string[]
+
+  likes?: number
+  comments?: number
+
+  createdAt: string
+}
