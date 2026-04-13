bd435248dedb6c39be68180d3ed4b5513d4c0e3e
Farhad S <amuzimteam@gmail.com>
2026-04-13 16:49:46 +0200

    Update group.ts

--- src/types/group.ts
+++ src/types/group.ts
@@ -1,1 +1,21 @@
 
+export interface Group {
+  id: string
+
+  name: string
+  description?: string
+
+  slug: string
+
+  category?:
+    | "genre"
+    | "city"
+    | "industry"
+    | "movement"
+
+  coverImage?: string
+
+  members?: number
+
+  createdAt: string
+}
