75bc2547d068b3afbfd989d2f552bbe7c3edf69a
Farhad S <amuzimteam@gmail.com>
2026-04-14 14:04:40 +0200

    feat(network): add NetworkProjects component

---
+++ src/components/network/NetworkProjects.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function NetworkProjects() {
+  return (
+    <div className="space-y-4">
+
+      <div className="border rounded-xl p-6">
+        Create Project
+      </div>
+
+      <div className="border rounded-xl p-6">
+        Open Collaborations
+      </div>
+
+      <div className="border rounded-xl p-6">
+        Active Projects
+      </div>
+
+    </div>
+  );
+}
