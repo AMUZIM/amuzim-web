a38151316f03675d1c7f9c971eeea17f37aeb635
Farhad S <amuzimteam@gmail.com>
2026-04-14 16:35:01 +0200

    feat(network): add create group component

--- src/components/network/NetworkGroups.tsx
+++ src/components/network/NetworkGroups.tsx
@@ -2,19 +2,24 @@
 
 import { networkGroups } from "@/data/networkGroups";
 import NetworkEmpty from "./NetworkEmpty";
+import NetworkCreate from "./NetworkCreate";
 
 export default function NetworkGroups() {
-  if (!networkGroups.length) {
-    return <NetworkEmpty title="No groups yet" />;
-  }
-
   return (
     <div className="space-y-4">
-      {networkGroups.map((group, index) => (
-        <div key={index} className="border rounded-xl p-6">
-          Group
-        </div>
-      ))}
+
+      <NetworkCreate title="Create Group" />
+
+      {!networkGroups.length ? (
+        <NetworkEmpty title="No groups yet" />
+      ) : (
+        networkGroups.map((group, index) => (
+          <div key={index} className="border rounded-xl p-6">
+            Group
+          </div>
+        ))
+      )}
+
     </div>
   );
 }
