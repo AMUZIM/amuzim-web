b49ce242765c0718b7d5429750bbf8b2546fe6a6
Farhad S <amuzimteam@gmail.com>
2026-04-14 16:33:31 +0200

    feat(network): add create project component

--- src/components/network/NetworkProjects.tsx
+++ src/components/network/NetworkProjects.tsx
@@ -2,19 +2,24 @@
 
 import { networkProjects } from "@/data/networkProjects";
 import NetworkEmpty from "./NetworkEmpty";
+import NetworkCreate from "./NetworkCreate";
 
 export default function NetworkProjects() {
-  if (!networkProjects.length) {
-    return <NetworkEmpty title="No projects yet" />;
-  }
-
   return (
     <div className="space-y-4">
-      {networkProjects.map((project, index) => (
-        <div key={index} className="border rounded-xl p-6">
-          Project
-        </div>
-      ))}
+
+      <NetworkCreate title="Create Project" />
+
+      {!networkProjects.length ? (
+        <NetworkEmpty title="No projects yet" />
+      ) : (
+        networkProjects.map((project, index) => (
+          <div key={index} className="border rounded-xl p-6">
+            Project
+          </div>
+        ))
+      )}
+
     </div>
   );
 }
