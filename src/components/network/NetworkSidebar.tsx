b6d339817da7b088d534fc36750da145eb194817
Farhad S <amuzimteam@gmail.com>
2026-04-14 05:52:55 +0200

    Update NetworkSidebar.tsx

--- src/components/network/NetworkSidebar.tsx
+++ src/components/network/NetworkSidebar.tsx
@@ -1,9 +1,10 @@
-
-import NetworkGroups from "./NetworkGroups";
+import NetworkGroups from "./NetworkGroups"
+import NetworkSignals from "./NetworkSignals"
 
 export default function NetworkSidebar() {
   return (
     <aside className="space-y-6">
+      <NetworkSignals />
       <NetworkGroups />
     </aside>
   );
