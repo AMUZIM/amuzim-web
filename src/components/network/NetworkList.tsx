ccba72475ae025331aecb78250e5975caa95aed5
Farhad S <amuzimteam@gmail.com>
2026-04-14 19:20:13 +0200

    feat(network): add NetworkList component

---
+++ src/components/network/NetworkList.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function NetworkList({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="space-y-4">
+      {children}
+    </div>
+  );
+}
