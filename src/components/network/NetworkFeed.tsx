f97c1402663ec469a0fb3a0857608fd063385044
Farhad S <amuzimteam@gmail.com>
2026-04-14 16:29:25 +0200

    feat(network): add create post component to feed

--- src/components/network/NetworkFeed.tsx
+++ src/components/network/NetworkFeed.tsx
@@ -2,19 +2,24 @@
 
 import { networkPosts } from "@/data/networkPosts";
 import NetworkEmpty from "./NetworkEmpty";
+import NetworkCreate from "./NetworkCreate";
 
 export default function NetworkFeed() {
-  if (!networkPosts.length) {
-    return <NetworkEmpty title="No posts yet" />;
-  }
-
   return (
     <div className="space-y-4">
-      {networkPosts.map((post, index) => (
-        <div key={index} className="border rounded-xl p-6">
-          Feed Item
-        </div>
-      ))}
+
+      <NetworkCreate title="Create Post" />
+
+      {!networkPosts.length ? (
+        <NetworkEmpty title="No posts yet" />
+      ) : (
+        networkPosts.map((post, index) => (
+          <div key={index} className="border rounded-xl p-6">
+            Feed Item
+          </div>
+        ))
+      )}
+
     </div>
   );
 }
