ef1b840dd1dc2ea9b716e87839b13c57a4fc5a34
Farhad S <amuzimteam@gmail.com>
2026-04-14 16:24:09 +0200

    feat(network): add empty state to groups

--- src/components/network/NetworkGroups.tsx
+++ src/components/network/NetworkGroups.tsx
@@ -1,21 +1,20 @@
 "use client";
 
 import { networkGroups } from "@/data/networkGroups";
+import NetworkEmpty from "./NetworkEmpty";
 
 export default function NetworkGroups() {
+  if (!networkGroups.length) {
+    return <NetworkEmpty title="No groups yet" />;
+  }
+
   return (
     <div className="space-y-4">
-
-      <div className="border rounded-xl p-6">
-        Create Group
-      </div>
-
       {networkGroups.map((group, index) => (
         <div key={index} className="border rounded-xl p-6">
           Group
         </div>
       ))}
-
     </div>
   );
 }
