dde820609684cfa6cada6aa3e181b918a07439ee
Farhad S <amuzimteam@gmail.com>
2026-04-14 16:46:32 +0200

    feat(network): add NetworkTabs component

---
+++ src/components/network/NetworkTabs.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function NetworkTabs() {
+  return (
+    <div className="flex gap-4 text-sm border rounded-xl p-2">
+      <div>All</div>
+      <div>Creators</div>
+      <div>Projects</div>
+      <div>Groups</div>
+    </div>
+  );
+}
