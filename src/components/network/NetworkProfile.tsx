1454568b5ac4ef93d7007a11f1ef91f0066b9b3f
Farhad S <amuzimteam@gmail.com>
2026-04-14 14:07:02 +0200

    feat(network): add NetworkProfile component

---
+++ src/components/network/NetworkProfile.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+export default function NetworkProfile() {
+  return (
+    <div className="space-y-4">
+
+      <div className="border rounded-xl p-6">
+        Profile Header
+      </div>
+
+      <div className="border rounded-xl p-6">
+        Skills
+      </div>
+
+      <div className="border rounded-xl p-6">
+        Active Projects
+      </div>
+
+      <div className="border rounded-xl p-6">
+        Collaboration History
+      </div>
+
+    </div>
+  );
+}
