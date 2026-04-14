a0376510872754c5ca6289789365aef1b8b41d5b
Farhad S <amuzimteam@gmail.com>
2026-04-14 16:26:24 +0200

    feat(network): add empty state to feed

--- src/components/network/NetworkFeed.tsx
+++ src/components/network/NetworkFeed.tsx
@@ -1,21 +1,20 @@
 "use client";
 
 import { networkPosts } from "@/data/networkPosts";
+import NetworkEmpty from "./NetworkEmpty";
 
 export default function NetworkFeed() {
+  if (!networkPosts.length) {
+    return <NetworkEmpty title="No posts yet" />;
+  }
+
   return (
     <div className="space-y-4">
-
-      <div className="border rounded-xl p-6">
-        Create Post
-      </div>
-
       {networkPosts.map((post, index) => (
         <div key={index} className="border rounded-xl p-6">
           Feed Item
         </div>
       ))}
-
     </div>
   );
 }
