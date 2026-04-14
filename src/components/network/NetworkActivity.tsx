0bca22a058c2a7b7663c311b71af42c75ef939be
Farhad S <amuzimteam@gmail.com>
2026-04-14 16:50:58 +0200

    feat(network): add NetworkActivity component

---
+++ src/components/network/NetworkActivity.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function NetworkActivity() {
+  return (
+    <div className="border rounded-xl p-4 space-y-2 text-sm">
+      <div>Trending Activity</div>
+      <div>New Collaborations</div>
+      <div>Industry Signals</div>
+    </div>
+  );
+}
