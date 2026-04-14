087370d1ab7fe8e059dae6696993f087f90bb302
Farhad S <amuzimteam@gmail.com>
2026-04-14 16:43:24 +0200

    feat(network): add NetworkFilters component

---
+++ src/components/network/NetworkFilters.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function NetworkFilters() {
+  return (
+    <div className="border rounded-xl p-4 space-y-2 text-sm">
+      <div>Creators</div>
+      <div>Projects</div>
+      <div>Groups</div>
+      <div>Signals</div>
+    </div>
+  );
+}
