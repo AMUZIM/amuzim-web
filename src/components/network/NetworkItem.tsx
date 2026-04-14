fd9a59da8389e72604f6f9d84535df8fba9049f8
Farhad S <amuzimteam@gmail.com>
2026-04-14 19:36:35 +0200

    feat(network): add actions to network items

--- src/components/network/NetworkItem.tsx
+++ src/components/network/NetworkItem.tsx
@@ -1,13 +1,25 @@
 "use client";
 
+import NetworkActions from "./NetworkActions";
+import NetworkButton from "./NetworkButton";
+
 export default function NetworkItem({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="border rounded-xl p-6">
-      {children}
+    <div className="border rounded-xl p-6 space-y-4">
+
+      <div>
+        {children}
+      </div>
+
+      <NetworkActions>
+        <NetworkButton>Connect</NetworkButton>
+        <NetworkButton>Message</NetworkButton>
+      </NetworkActions>
+
     </div>
   );
 }
