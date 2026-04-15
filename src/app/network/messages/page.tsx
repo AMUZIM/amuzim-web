e102b059e838621da6e0dc12f6b5435b43970118
Farhad S <amuzimteam@gmail.com>
2026-04-15 19:06:56 +0200

    fix(network): disable messages page to unblock build

--- src/app/network/messages/page.tsx
+++ src/app/network/messages/page.tsx
@@ -1,11 +1,5 @@
 "use client";
 
-import NetworkThreadView from "@/components/network/NetworkThreadView";
-
 export default function MessagesPage() {
-  return (
-    <div className="p-6">
-      <NetworkThreadView threadId="demo_thread" />
-    </div>
-  );
+  return <div />;
 }
