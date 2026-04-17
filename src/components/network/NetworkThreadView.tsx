b03c7141d800559254d1b97c596d4945cdd32792
Farhad S <amuzimteam@gmail.com>
2026-04-17 07:55:51 +0200

    style(network): refine horizontal spacing for message rows

--- src/components/network/NetworkThreadView.tsx
+++ src/components/network/NetworkThreadView.tsx
@@ -78,7 +78,7 @@ const NetworkThreadView = forwardRef<ThreadViewRef, Props>(
           return (
           <div
            key={m.id}
-           className={`flex w-full px-2 ${
+           className={`flex w-full px-3 ${
              isMe ? "justify-end" : "justify-start"
            }`}
           >
