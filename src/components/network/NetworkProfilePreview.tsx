b0eb66541a69bf227e15396e47f1767cccd6c0ba
Farhad S <amuzimteam@gmail.com>
2026-04-19 01:22:10 +0200

    style(network): truncate location text in ProfilePreview

--- src/components/network/NetworkProfilePreview.tsx
+++ src/components/network/NetworkProfilePreview.tsx
@@ -24,7 +24,7 @@ export default function NetworkProfilePreview({ profile }: Props) {
         )}
 
         {profile.location && (
-          <div className="text-xs text-gray-400 leading-tight">
+          <div className="text-xs text-gray-400 leading-tight truncate">
             {profile.location}
           </div>
         )}
