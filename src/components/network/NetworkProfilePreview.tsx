cc3b1f54593035fa1ea516c7c2066d1710d6f7f6
Farhad S <amuzimteam@gmail.com>
2026-04-19 01:59:19 +0200

    style(network): refine bio padding in ProfilePreview

--- src/components/network/NetworkProfilePreview.tsx
+++ src/components/network/NetworkProfilePreview.tsx
@@ -18,7 +18,7 @@ export default function NetworkProfilePreview({ profile }: Props) {
         </div>
 
         {profile.bio && (
-           <div className="text-xs text-gray-500 leading-tight line-clamp-2 break-words tracking-tight">
+           <div className="text-xs text-gray-500 leading-tight line-clamp-2 break-words tracking-tight px-1">
              {profile.bio}
            </div>
         )}
