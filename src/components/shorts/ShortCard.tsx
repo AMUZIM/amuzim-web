6c6053f17150343e94b66b5d3e63cbc28f5ace7e
Farhad S <amuzimteam@gmail.com>
2026-04-19 18:56:39 +0200

    feat(shorts): add InteractionBar and integrate into ShortCard

--- src/components/shorts/ShortCard.tsx
+++ src/components/shorts/ShortCard.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import VideoPlayer from "./VideoPlayer";
+import InteractionBar from "./InteractionBar";
 
 type Props = {
   src: string;
@@ -11,6 +12,7 @@ export default function ShortCard({ src, isActive }: Props) {
   return (
     <div className="h-screen w-full relative bg-black">
       <VideoPlayer src={src} isActive={isActive} />
+      <InteractionBar />
     </div>
   );
 }
