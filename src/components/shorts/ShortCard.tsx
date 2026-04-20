72701361f446acc83d1b8fcf5f5123752148dadd
Farhad S <amuzimteam@gmail.com>
2026-04-20 02:56:05 +0200

    fix(shorts): add preload prop to ShortCard

--- src/components/shorts/ShortCard.tsx
+++ src/components/shorts/ShortCard.tsx
@@ -6,12 +6,13 @@ import InteractionBar from "./InteractionBar";
 type Props = {
   src: string;
   isActive: boolean;
+  preload?: boolean;
 };
 
-export default function ShortCard({ src, isActive }: Props) {
+export default function ShortCard({ src, isActive, preload }: Props) {
   return (
     <div className="h-screen w-full relative bg-black">
-      <VideoPlayer src={src} isActive={isActive} />
+      <VideoPlayer src={src} isActive={isActive} preload={preload} />
       <InteractionBar />
     </div>
   );
