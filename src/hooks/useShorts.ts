33d31fbe848d25001c17cdb97a06f8463958c6c5
Farhad S <amuzimteam@gmail.com>
2026-04-20 03:33:31 +0200

    feat(shorts): add mock data hook for shorts

--- src/hooks/useShorts.ts
+++ src/hooks/useShorts.ts
@@ -1,6 +1,19 @@
 export function useShorts() {
   return {
-    data: [],
+    data: [
+      {
+        id: "1",
+        mediaUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
+      },
+      {
+        id: "2",
+        mediaUrl: "https://www.w3schools.com/html/movie.mp4",
+      },
+      {
+        id: "3",
+        mediaUrl: "https://www.w3schools.com/html/mov_bbb.mp4",
+      },
+    ],
     loading: false,
   };
 }
