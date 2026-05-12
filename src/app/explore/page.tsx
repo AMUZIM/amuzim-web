b7b92176f5bf82f2a17da61a70189a5f592c53da
Farhad S <amuzimteam@gmail.com>
2026-05-12 07:04:39 +0200

    refactor(explore): prepare new beta explore structure

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,8 +1,9 @@
-import HeroExplore from "../../components/HeroExplore"
-import CuratedMusic from "../../components/CuratedMusic"
-import MusicLovers from "../../components/MusicLovers"
-import Learning from "../../components/Learning"
-import ExploreCTA from "../../components/ExploreCTA"
+import QuickEntrySection from "../../components/QuickEntrySection"
+import FeaturedSection from "../../components/FeaturedSection"
+import EarlyAccessSection from "../../components/EarlyAccessSection"
+import DiscoverSection from "../../components/DiscoverSection"
+import FutureSection from "../../components/FutureSection"
+import EditorialEntrySection from "../../components/EditorialEntrySection"
 
 export default function Explore() {
   return (
