e7f73f49544febf69f8fcfc39bc5a0833664968d
Farhad S <amuzimteam@gmail.com>
2026-05-27 04:02:42 +0200

    refactor(shared): centralize route type usage

--- src/shared/utils/get-route-type.ts
+++ src/shared/utils/get-route-type.ts
@@ -1,10 +1,12 @@
+import type { RouteType } from "@/shared/types/route-type";
+
 import { isAuthRoute } from "./is-auth-route";
 import { isDashboardRoute } from "./is-dashboard-route";
 import { isPublicRoute } from "./is-public-route";
 
 export function getRouteType(
   pathname: string
-) {
+): RouteType {
   if (isAuthRoute(pathname)) {
     return "auth";
   }
