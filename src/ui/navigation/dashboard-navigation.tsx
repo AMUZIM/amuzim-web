fd44a12007e436490a01a2d6109ac050a59fadd5
Farhad S <amuzimteam@gmail.com>
2026-05-27 04:07:52 +0200

    refactor(ui): centralize dashboard navigation items

--- src/ui/navigation/dashboard-navigation.tsx
+++ src/ui/navigation/dashboard-navigation.tsx
@@ -1,46 +1,18 @@
 import Link from "next/link";
 
-import { ROUTES } from "@/shared/constants/routes";
+import { DASHBOARD_NAVIGATION } from "@/shared/constants/dashboard-navigation";
 
 export default function DashboardNavigation() {
   return (
     <nav>
       <ul>
-        <li>
-          <Link href={ROUTES.PROFILE}>Profile</Link>
-        </li>
-
-        <li>
-          <Link href={ROUTES.SETTINGS}>Settings</Link>
-        </li>
-
-        <li>
-          <Link href={ROUTES.CREATOR}>Creator</Link>
-        </li>
-
-        <li>
-          <Link href={ROUTES.NETWORK}>Network</Link>
-        </li>
-
-        <li>
-          <Link href={ROUTES.EDITORIAL}>Editorial</Link>
-        </li>
-
-        <li>
-          <Link href={ROUTES.EXPLORE}>Explore</Link>
-        </li>
-
-        <li>
-          <Link href={ROUTES.STREAMING}>Streaming</Link>
-        </li>
-
-        <li>
-          <Link href={ROUTES.FUTURE}>Future</Link>
-        </li>
-
-        <li>
-          <Link href={ROUTES.ANALYTICS}>Analytics</Link>
-        </li>
+        {DASHBOARD_NAVIGATION.map((item) => (
+          <li key={item.href}>
+            <Link href={item.href}>
+              {item.label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
