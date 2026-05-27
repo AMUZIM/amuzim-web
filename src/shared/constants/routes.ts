4ef957432f6e7e65d5b6cf0036ee3bc6b5a56b20
Farhad S <amuzimteam@gmail.com>
2026-05-27 02:29:36 +0200

    feat(shared): prepare shared routes constants

---
+++ src/shared/constants/routes.ts
@@ -0,0 +1,17 @@
+export const ROUTES = {
+  HOME: "/",
+  AUTH: "/auth",
+
+  DASHBOARD: "/dashboard",
+
+  PROFILE: "/dashboard/profile",
+  SETTINGS: "/dashboard/settings",
+
+  CREATOR: "/dashboard/creator",
+  NETWORK: "/dashboard/network",
+  EDITORIAL: "/dashboard/editorial",
+  EXPLORE: "/dashboard/explore",
+  STREAMING: "/dashboard/streaming",
+  FUTURE: "/dashboard/future",
+  ANALYTICS: "/dashboard/analytics",
+};
