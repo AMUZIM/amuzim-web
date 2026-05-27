7b6505d6262aab57807c65aa311c01918a4d5747
Farhad S <amuzimteam@gmail.com>
2026-05-27 02:10:53 +0200

    feat(dashboard): prepare dashboard navigation foundation

--- src/app/(protected)/dashboard/layout.tsx
+++ src/app/(protected)/dashboard/layout.tsx
@@ -1,7 +1,53 @@
+import Link from "next/link";
+
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <section>{children}</section>;
+  return (
+    <main>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/dashboard/profile">Profile</Link>
+          </li>
+
+          <li>
+            <Link href="/dashboard/settings">Settings</Link>
+          </li>
+
+          <li>
+            <Link href="/dashboard/creator">Creator</Link>
+          </li>
+
+          <li>
+            <Link href="/dashboard/network">Network</Link>
+          </li>
+
+          <li>
+            <Link href="/dashboard/editorial">Editorial</Link>
+          </li>
+
+          <li>
+            <Link href="/dashboard/explore">Explore</Link>
+          </li>
+
+          <li>
+            <Link href="/dashboard/streaming">Streaming</Link>
+          </li>
+
+          <li>
+            <Link href="/dashboard/future">Future</Link>
+          </li>
+
+          <li>
+            <Link href="/dashboard/analytics">Analytics</Link>
+          </li>
+        </ul>
+      </nav>
+
+      {children}
+    </main>
+  );
 }
