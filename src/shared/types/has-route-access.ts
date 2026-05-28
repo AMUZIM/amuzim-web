cf2e707a8075cec70c66e3a7d5e3a653210d541c
Farhad S <amuzimteam@gmail.com>
2026-05-28 06:53:53 +0200

    fix(imports): resolve shared utils aliases

--- src/shared/types/has-route-access.ts
+++ src/shared/types/has-route-access.ts
@@ -1,5 +1,5 @@
-import { canAccessRoute } from "./can-access-route";
-import { getUserRole } from "./get-user-role";
+import { canAccessRoute } from "@/shared/utils/can-access-route";
+import { getUserRole } from "@/shared/utils/get-user-role";
 
 export function hasRouteAccess(pathname: string) {
   const role = getUserRole();
